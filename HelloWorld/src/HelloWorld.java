b8ae37e55772adce0fac002b1c7d8d5302d257b9
Wilson Sun <WilsonSun@hotmail.com>
2013-09-09 22:24:04 -0400

    Add test DTaaS 1

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -8,6 +8,7 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world!");		
 		System.out.println("Test 1");
+		System.out.println("Test DTaaS 1");
 	}
 
 }
